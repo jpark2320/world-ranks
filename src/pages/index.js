047eb6fdae4da4025a948ed22bf964e8b371bc1b
jpark2320 <jpark2320@gmail.com>
2020-11-27 04:57:44 -0500

    Create SearchInput component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import Layout from 'components/Layout/Layout';
+import SearchInput from 'components/SearchInput/SearchInput';
+import styles from 'styles/Home.module.css';
 
-export default function Home() {
-  return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} contries</div>
+
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+};
